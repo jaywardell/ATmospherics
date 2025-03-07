4120c24422544126e8d27cfb7e210a7ce323a23c
Joseph Wardell <jay@jaywardell.me>
2025-03-07 12:18:52 -0500

    added very light documentation

--- Sources/ATmospherics/ProfileRetriever.swift
+++ Sources/ATmospherics/ProfileRetriever.swift
@@ -6,12 +6,15 @@
 //
 
 public struct ProfileRetriever: Sendable {
+    
+    /// the handle of a ATProto profile
     let handle: String
     
     public init(handle: String) {
         self.handle = handle
     }
     
+    /// retrieves data about the ATProto profile stored in `handle`
     public func retrieveProfile(using atmosphere: Atmosphere = .uncredentialed) async throws -> Profile {
         
         let kit = try await atmosphere.atProto()
