adffcf5b43b1f5f2c17f2399411e7e59098a0227
Joseph Wardell <jay@jaywardell.me>
2025-03-11 09:41:09 -0400

    added tests to verify that a session is nil unless valid credentials are provided

--- Tests/ATmosphericsTests/ATmosphericsTests.swift
+++ Tests/ATmosphericsTests/ATmosphericsTests.swift
@@ -1,6 +1,41 @@
 import Testing
 @testable import ATmospherics
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Suite("Atmosphere")
+struct AtmosephereTests {
+    
+    @Test("Behavior of uncredentialed Atmosphere")
+    func example() async throws {
+        
+        let sut = Atmosphere.uncredentialed
+        
+        let proto = try #require(try await sut.atProto())
+        
+        #expect(nil == proto.session)
+    }
+
+    @Test("Behavior of credentialed Atmosphere")
+    func example2() async throws {
+        let sut = Atmosphere(credential: .testingAccount)
+        
+        do {
+            let proto = try await sut.atProto()
+            #expect(nil != proto.session)
+            #expect(false == proto.session?.accessToken.isEmpty)
+            #expect(proto.session?.handle == "atmosphericstests.bsky.social")
+        }
+        catch {
+            print(error.localizedDescription)
+            print()
+            print(error)
+            #expect(1 + 1 == 3)
+        }
+    }
+}
+
+extension Atmosphere.Credential {
+    // an account that can be used for testing purposes
+    // yes, I'm releasing this on the web
+    // please don't abuse
+    static let testingAccount = Atmosphere.Credential(handle: "jay+bluesky_testing@jaywardell.me", appPassword: "vokdib-koxBo4-huczyz")
 }
