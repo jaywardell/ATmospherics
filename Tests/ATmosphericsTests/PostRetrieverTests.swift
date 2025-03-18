d189c5321bd000922d21097ba3e2bd8fac0b7f4b
Joseph Wardell <jay@jaywardell.me>
2025-03-18 09:46:28 -0400

    added a test to ensure that posts with limited interactivity are still viewable

--- Tests/ATmosphericsTests/PostRetrieverTests.swift
+++ Tests/ATmosphericsTests/PostRetrieverTests.swift
@@ -54,6 +54,12 @@ struct retrievePostURL {
     // has been turned on for its account
     private let knownPrivatePost = URL(string: "https://bsky.app/profile/atmosphericstests.bsky.social/post/3lknpurb4nc23")!
     
+    // This post has limited interactivity
+    // it's marked as only being replyable by followers
+    // but it should still be viewable
+    // since only interactions are blocked, not viewing
+    private let knownLimitedInteractivePost = URL(string: "https://bsky.app/profile/skymarks.bsky.social/post/3lknsaddvqc2h")!
+    
     @Test("Throws if given uncredentialed atmopsphere")
     func throws_if_not_credentialed() async throws {
         let sut = PostRetriever()
@@ -80,5 +86,24 @@ struct retrievePostURL {
             Issue.record("failed with error \(error)")
         }
     }
+
+    @Test("Retrieves Post given URL for post with limited interaction")
+    func retrieve_post_limited_interaction() async throws {
+        let atmosphere = Atmosphere(credential: .testingAccount)
+
+        let sut = PostRetriever()
+        do {
+            let post = try await sut.retrievePost(for: knownLimitedInteractivePost, using: atmosphere)
+            #expect(nil != post)
+            #expect(post?.uri.components(separatedBy: "/").last == knownLimitedInteractivePost.lastPathComponent)
+            #expect(post?.text == "Coming soon hopefully, the ability to bookmark a private post (assuming that your account can view it)")
+        }
+        catch {
+            print(error.localizedDescription)
+            print(error)
+            Issue.record("failed with error \(error)")
+        }
+    }
+
 
 }
